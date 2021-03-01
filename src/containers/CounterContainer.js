5d653a876cf0789adefc4ae73a6bff8bd7853e75
wyjeon <wyjeon.info@gmail.com>
2021-03-01 14:54:04 +0900

    프로젝트 세팅

---
+++ src/containers/CounterContainer.js
@@ -0,0 +1,17 @@
+import React, { useCallback } from 'react';
+import Counter from '../components/Counter';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrease, increase } from '../modules/counter';
+
+function CounterContainer() {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return (
+    <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
+  );
+}
+
+export default CounterContainer;
